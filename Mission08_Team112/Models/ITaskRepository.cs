704b30cafc5d946ef8c7dd62a3a2d8b805f3310a
andrewhmg <120764533+andrewhmg@users.noreply.github.com>
2026-02-25 10:43:36 -0700

    added 2 more routes

--- Mission08_Team112/Models/ITaskRepository.cs
+++ Mission08_Team112/Models/ITaskRepository.cs
@@ -8,4 +8,9 @@ namespace Mission08_Team112.Models;
         public void AddTask(Task task);
         public void UpdateTask(Task task);
         public void DeleteTask(Task task);
+
+        public Task GetTaskById(int id);
+        
+        // NEW: Mark a task as completed (can be done inside UpdateTask, but a helper is nice)
+        public void MarkTaskComplete(int id);
     }
