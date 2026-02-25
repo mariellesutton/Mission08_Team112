5b816e9da478a6b70d3f77b897a57645d87a4825
andrewhmg <120764533+andrewhmg@users.noreply.github.com>
2026-02-25 09:59:38 -0700

    Made the models for the project

---
+++ Mission08_Team112/Models/Task.cs
@@ -0,0 +1,33 @@
+namespace Mission08_Team112.Models;
+
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Task name is required")]
+        public string TaskName { get; set; }
+
+        public string? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Quadrant is required")]
+        [Range(1, 4, ErrorMessage = "Quadrant must be between 1 and 4")]
+        public int Quadrant { get; set; }
+
+        // Foreign Key Relationship
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public bool Completed { get; set; } = false;
+    }
+}
